f415330d3d8a32d015cb0d23e58e7fdf690521af
yadav767 <yyadavaayush767@gmail.com>
2026-02-27 15:00:07 +0530

    Changes1

--- src/Components/Loader.jsx
+++ src/Components/Loader.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Loader = () => {
   return (
-    <div className='h-screen flex items-center justify-center px-6 z-[100]  fixed inset-0 bg-white'>
-        <div className="flex font-semibold max-sm:text-4xl">
+    <div className='h-screen flex items-center justify-center  z-[100]  fixed inset-0 bg-white'>
+        <div className="flex font-semibold max-sm:text-4xl px-3">
             <div className='text-5xl text-black first'>Evolve</div>
             <div className='text-5xl text-black second'>With</div>
             <div className='text-5xl text-black third'>Rahul</div>
